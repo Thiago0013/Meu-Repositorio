69514f114e0bb22e45737b1877b3c759a2ef5975
Ian Thiago <130786947+Thiago0013@users.noreply.github.com>
2024-09-05 22:43:24 -0300

    Adicionando conteúdo a página

--- scripts/main.js
+++ scripts/main.js
@@ -4,3 +4,17 @@ function contatos(){
 function retornar(){
     window.location.href = "index.html";
 }
+function entrar(elemento){
+    var id = elemento.id;
+
+    switch (id) {
+        case "instagram":
+            window.open("https://www.instagram.com/03thiagoh/", "_blank");
+            break;
+        case "github":
+            window.open("https://github.com/thiago0013", "_blank");
+            break;
+        default:
+            console.log("ID não reconhecido: " + id);
+    }
+}
